ac62a9a89afda45ecb67d27f993e6765b48862b6
Erik Markhauser <emarkhauser@yahoo.com>
2016-01-02 20:52:45 -0500

    Change Collection Type from ArrayList to LinkedList for Employers.  Make EmployerService class more efficiently programmed.

--- src/main/java/ca/markhauser/service/EmployerService.java
+++ src/main/java/ca/markhauser/service/EmployerService.java
@@ -1,37 +1,55 @@
 package ca.markhauser.service;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
 import ca.markhauser.model.CompanyResponse;
 import ca.markhauser.model.Employer;
 
+@Service
 public class EmployerService {
 
 	private String glassdoorApiUrl = "http://api.glassdoor.com/api/api.htm?&userip=0.0.0.0&useragent=&format=json";
 	private String glassdoorUrlEmployerBase = glassdoorApiUrl
 			+ "&v=1&action=employers";
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(EmployerService.class);
+
 	public EmployerService() {
 	}
 
-	public ArrayList<Employer> getEmployers(String partnerId,
+	public LinkedList<Employer> getEmployers(String partnerId,
 			String partnerKey, String location) {
-		RestTemplate restTemplate = new RestTemplate();
-		ArrayList<Employer> employers = new ArrayList<Employer>();
-		int pageNumber = 0;
-		int currentPageNumber = 0;
-		int totalNumberOfPages = 0;
+		
+		LinkedList<Employer> employers = new LinkedList<Employer>();
+		int i = 1;
+		int totalNumberPages = 0;
+
 		do {
-			pageNumber++;
-			CompanyResponse companyResponse = restTemplate.getForObject(
-					glassdoorUrlEmployerBase + "&t.p=" + partnerId + "&t.k="
-							+ partnerKey + "&l=" + location + "&pn="
-							+ pageNumber, CompanyResponse.class);
-			currentPageNumber = companyResponse.getResponse().getCurrentPageNumber();
-			totalNumberOfPages = companyResponse.getResponse().getTotalNumberOfPages();
+			CompanyResponse companyResponse = getCompanyResponseByPage(partnerId, partnerKey, location, i);
 			employers.addAll(companyResponse.getResponse().getEmployers());
-		} while (currentPageNumber < totalNumberOfPages);
+			totalNumberPages = companyResponse.getResponse().getTotalNumberOfPages();
+			i ++;
+		} while (i <= totalNumberPages);
+
 		return employers;
+	}
+
+	private CompanyResponse getCompanyResponseByPage(String partnerId,
+			String partnerKey, String location, int pageNumber) {
+		RestTemplate restTemplate = new RestTemplate();
+		logger.info("Page Number: " + pageNumber + ": Rest Template object created");
+		CompanyResponse companyResponse = restTemplate.getForObject(
+				glassdoorUrlEmployerBase + "&t.p=" + partnerId + "&t.k="
+						+ partnerKey + "&l=" + location + "&pn=" + pageNumber,
+				CompanyResponse.class);
+		logger.info("Page Number: " + pageNumber + ": Received REST response for page");
+		return companyResponse;
 	}
 
 }
